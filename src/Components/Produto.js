121a6a1c0310a279084271a773011e9d9e028b7d
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-25 15:58:58 -0500

    Finalizada a parte de Produtos, onde eu utilizei uma API pronta de um curso, e estou mostrando na tela todos os produtos disponíveis, agora estou implementando a tela de produto, onde após clicar na imagem de um produto na primeira tela, o usuário poderá ver mais informações sobre aquele produto especifico!

---
+++ src/Components/Produto.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Head from './Head';
+import styles from './Styles/Produto.module.css'
+
+function Produto() {
+  const [produto, setProduto] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null)
+  const {id} = useParams();
+  console.log(id)
+
+  React.useEffect(() => {
+    async function reqProduto(url){
+      try{
+        setLoading(true)
+        const res = await axios.get(url)
+        setProduto(res.data)
+        console.log(res.data)
+      }catch(erro){
+        setError('Um erro ocorreu')
+      }finally{
+        setLoading(false)
+      }
+      
+    }
+    reqProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`)
+  },[id])
+
+  if(loading) return <div>Carregando...</div>
+  if(error) return <p>{error}</p>
+  if(produto === null) return null
+
+  return (
+    <section className={styles.produto + ' animeLeft'}>
+      <Head title={produto.nome} descricao={`Essa é a página do produto ${produto.nome}`}/>
+      {produto.fotos.map(foto => <img key={foto.src} src={foto.src} alt={foto.titulo}></img>)}
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={styles.preco}>R$ {produto.preco}</span>
+        <p className={styles.descricao}>{produto.descricao}</p>
+      </div>
+    </section>
+  )
+}
+
+export default Produto
--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import styles from './Styles/Produtos.module.css'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 import Head from './Head'
 
 function Produtos() {
+
+  const [produtos, setProdutos] = React.useState(null)
+
+   React.useEffect(() => {
+   axios.get('https://ranekapi.origamid.dev/json/api/produto').then((response) => setProdutos(response.data))
+  }, [])
+
+  if(produtos === null) return null
+
   return (
-    <div>
-      <Head title='Produtos' descricao='Esta é a página de produtos'/>
-      <h1>Produtos</h1>
-    </div>
+    <section className={styles.produtos + ' animeLeft'}>
+      <Head title='WD Store'/>
+      {produtos.map((p) => (<Link to={`produto/${p.id}`} key={p.id}>
+      <h1 className={styles.nome}>{p.nome}</h1>
+      <img src={p.fotos[0].src} alt={p.title}></img>
+    </Link>))}
+    </section>
   )
 }
 
