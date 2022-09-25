c82ccc7130737468cfcf0a511f6c00f8d2eb9dfb
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-25 15:11:03 -0500

    Finalizada por enquanto a página de contato, talvez ainda surjam alterações conforme eu mudo o projeto, adicionei a alteração do titulo da página conforme o usuário navega com os links

--- src/Components/Contato.js
+++ src/Components/Contato.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import styles  from './Styles/Contato.module.css'
+import foto from '../contato.jpg'
+import Head from './Head'
+
 function Contato() {
   return (
-    <div>
-      <h1>Contato</h1>
-    </div>
+    <section className={styles.contato + ' animeLeft'}>
+      <Head title='Contato' descricao='Entre em contato'/>
+      <img src={foto} alt='Maquina de escrever'></img>
+      <div>
+        <h1>Entre em contato</h1>
+        <ul className={styles.dados}>
+          <li>
+          <ion-icon name="mail-sharp"></ion-icon>
+            <span className={styles.texto}>wesley.dias.dev@gmail.com</span>
+          </li>
+          <li>
+          <ion-icon name="logo-linkedin"></ion-icon>
+            <span className={styles.texto}>https://www.linkedin.com/in/wesleydiasdev/</span>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './Styles/Produtos.module.css'
+import Head from './Head'
 
 function Produtos() {
   return (
     <div>
+      <Head title='Produtos' descricao='Esta é a página de produtos'/>
       <h1>Produtos</h1>
     </div>
   )
